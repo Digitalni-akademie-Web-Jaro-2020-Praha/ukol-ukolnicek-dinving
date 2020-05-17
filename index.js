3f4e017d9adb5907fa2c9552e44f41ba2b2a6e57
Daniela <dinving@gmail.com>
2020-05-17 10:27:02 +0200

    druha cast ukolu, tlacitko

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 'use strict';
 
 console.log('funguju!');
-//Smažte obsah elementu todo__tasks uvnitř souboru index.html.\\
-//V JavaScriptu vytvořte pole řetězců tasks, které bude reprezentovat seznam úkolů.\\
-//Vytvořte funkci updateTasks, která zobrazí na stránce seznam úkolů dle toho, co je obsaženo v poli tasks.
-//Umožněte uživateli přidat nový úkol pomocí textového políčka. Vždy, když uživatel klikne tlačítko přidat, vložte do pole tasks obsah textového políčka a zavolejte funkci updateTasks, která aktualizuje vaši stránku.\\
-
-//<div class="todo__tasks">
-// <div class="task">
-//    Vyprat ponožky
-//       </div>
-//  <div class="task">
-//   Naučit se funkce vyššího řádu
-//        </div>
-//  <div class="task">
-//    Nakoupit na víkend
-//        </div>
-//</div>\\
 
 const tasks = ['Nalít si víno', 'Vyvenčit psa', 'Upéct chleba', 'Dát si nohy na stůl',]
 const updateTasks = () => {
@@ -28,10 +12,9 @@ const updateTasks = () => {
 }
 updateTasks()
 
-
-function newTask() {
-  const addedTask = document.getElementById("new-task").value;
-  tasks.push(addedTask);
+const newTask = () => {
+  const addTask = document.getElementById("new-task").value;
+  tasks.push(addTask);
   updateTasks(tasks);
   document.getElementById("new-task").value = "";
 }
